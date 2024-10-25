09b2a46f470dd402c1703a13115aaa2fe747e785
Krishnan <sunilkrish2004@gmail.com>
2024-10-25 09:52:44 +0530

    design correction

--- src/Notify.jsx
+++ src/Notify.jsx
@@ -5,7 +5,7 @@ const Notify = ({notify, setNotify }) => {
     return (
         <div>
             {notify&&
-            <div className="p-4 flex flex-col items-center fixed top-0 left-1/2 z-10 bg-white border border-black rounded-xl">
+            <div className="p-4 flex flex-col items-center fixed top-0 left-10 sm:left-1/2 z-10 bg-white border border-black rounded-xl">
                 <p className="text-xl">You are not authenticate ? </p>
                 <div className="flex gap-2">
                     <button id="button" onClick={()=>setNotify(false)}>cancel</button>
