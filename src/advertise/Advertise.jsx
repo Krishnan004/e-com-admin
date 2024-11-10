f3920a21b29a95bef1092aa8e68e17b081924775
Krishnan <sunilkrish2004@gmail.com>
2024-11-10 21:39:24 +0530

    backend api changed

--- src/advertise/Advertise.jsx
+++ src/advertise/Advertise.jsx
@@ -22,7 +22,7 @@ function Advertise() {
             <div className="bg-white rounded-xl sm:w-1/3 space-y-2 font-semibold">
                 <h1 className="font-bold m-4">Product no: {item.product_id}</h1>
                 <div className="flex justify-around border-b-2 p-2 sm:p-0" >
-                    <img src={`https://e-com-server-1-9p85.onrender.com/upload/${item.product_src}`} alt="" className="w-36" />
+                    <img src={`https://e-com-server-2.onrender.com/upload/${item.product_src}`} alt="" className="w-36" />
                     <div>
                         <p>{item.product_name}</p>
                         <p>{item.design}</p>
